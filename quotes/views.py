d93478043d95a554e84094704d8bc17fe4954a3b
Lance Sinson <ssinson@bu.edu>
2025-02-05 15:59:57 -0500

    working on css assignment 3

--- quotes/views.py
+++ quotes/views.py
@@ -7,8 +7,16 @@ import time
 import random
 # Create your views here.
 
-quotes_list = []
-images_list = []
+QUOTES = [
+    "A man paints with his brains and not with his hands",
+    "Genius is eternal patience",
+    "If people knew how hard I worked to get my mastery, it wouldn't seem so wonderful at all",
+]
+IMAGES = [
+    "../../static/michelangelo_1.jpg",
+    "../../static/michelangelo_2.jpeg",
+    "../../static/michelangelo_3.jpg",
+]
 
 def quote(request):
 
@@ -16,18 +24,32 @@ def quote(request):
     # a dict of context variables (key-value pairs)
     context = {
         "time": time.ctime(),
-        # "letter1": chr(random.randint(65,90)),
-        # "letter2": chr(random.randint(65,90)),
-        # "number": random.randint(1,10),
+        "quote": random.choice(QUOTES),
+        "image": random.choice(IMAGES),
     }
     return render(request, template_name, context)
 
 
 
 def show_all(request):
-    return HttpResponse("Hello, show_all!")
+    template_name = 'quotes/show_all.html'
+
+    context = {
+        "time": time.ctime(),
+        "quotes": QUOTES,
+        "images": IMAGES,
+    }
+    return render(request, template_name, context)
 
 
 
 def about(request):
-    return HttpResponse("Hello, about!")
+    template_name = 'quotes/about.html'
+
+    context = {
+            "time": time.ctime(),
+            "quotes": QUOTES,
+            "images": IMAGES,
+        }
+
+    return render(request, template_name, context)

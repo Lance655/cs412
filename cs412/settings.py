5c75bea088940a8fe48a39ed3d25fbd38f9808cc
Lance Sinson <ssinson@bu.edu>
2025-02-09 23:30:35 -0500

    week4videos

--- cs412/settings.py
+++ cs412/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "hw", # NEW
     "quotes",
+    "formdata", # NEW
 ]
 
 MIDDLEWARE = [
--- cs412/urls.py
+++ cs412/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("hw/", include("hw.urls")),
-    path("quotes/", include("quotes.urls")) # new
+    path("quotes/", include("quotes.urls")), 
+    path("formdata/", include("formdata.urls")), ## NEW
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

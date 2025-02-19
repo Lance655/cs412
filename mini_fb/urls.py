1502baa1d9fbe1f16fc9bc9a6d38ff658bc27062
Lance Sinson <ssinson@bu.edu>
2025-02-18 23:46:18 -0500

    finished task1 assignment5

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -3,8 +3,8 @@
 # Description: The urls file for the mini_fb website. 
 
 from django.urls import path
-
+from .views import ShowAllProfilesView
 
 urlpatterns = [
-
+    path('', ShowAllProfilesView.as_view(), name="show_all_profiles"),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,18 @@
+# File: views.py
+# Author: Lance Sinson (ssinson@bu.edu), 2/21/25
+# Description: The views file for the mini_fb website. 
+
+# mini_fb/views.py
+# views for the mini_fb application
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Profile
+import random
 
 # Create your views here.
+class ShowAllProfilesView(ListView):
+    '''Define a view class to show all mini_fb profiles.'''
+
+    model = Profile
+    template_name = "mini_fb/show_all_profiles.html"
+    context_object_name = "profiles" # note plural variable name
